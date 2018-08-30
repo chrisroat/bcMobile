9652c8245e0fe7a9dfd03ccd1c6831d90e07f6a1
Joshua Paik <joshuapaik@Joshuas-MBP.fios-router.home>
2018-08-30 12:35:18 -0700

    iphonex

--- sceneHome.js
+++ sceneHome.js
@@ -9,7 +9,7 @@ var sceneHome = function(p) {
     p.background('#ccffff')
     p.fill('black');
 
-    playButton = p.createButton('Play').position(p.displayWidth/2-25, p.displayHeight-180)
+    playButton = p.createButton('Play').position(p.displayWidth/2-25, p.displayHeight-400)
     playButton.style('width', '60px')
         .style('height', '35px')
         .style('background-color', "#3399ff")
@@ -27,10 +27,6 @@ var sceneHome = function(p) {
     p.textFont(myFont)
     p.text('Broken Calculators',(p.displayWidth/2)-120,(p.displayHeight/2)-200);
 
-    //Menu bar
-    p.noStroke()
-    p.fill('lightgrey');
-    p.rect(0,p.displayHeight-200,p.displayWidth,200)
   }
 
   function switchToLevels(){
